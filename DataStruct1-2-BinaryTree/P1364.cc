6354ebe250ae9d6b916f5bb0a60f6925c3cfbbc8
ASENNIU <1334575226@qq.com>
2023-10-22 17:36:14 +0800

    补题。树上DP和更好的建图

--- DataStruct1-2-BinaryTree/P1364.cc
+++ DataStruct1-2-BinaryTree/P1364.cc
@@ -1,6 +1,14 @@
 //
 // Created by 13345 on 2023/10/19.
 //
+
+/***
+ * 建图的方式：边去记录上一条边的信息，节点数组记录最后一条边的编号
+ * 树上DP，以任意节点开始假设为重心（标记为root），求dist，那么重心在转移时，距离的变化有两项
+ * 1. old_root的子节点距离会减少，减少的部分是各个子节点的权重和，对一个子节点而言dist的变化 weight * depth -> weight * (depth - 1)
+ * 2. new_root的父节点的距离会增加，增加的部分是父节点的权重和
+ * 3. 通过树上前缀和size来记录以root为根的各级节点的权重和，size[root] - size[u]，就可以表示出节点u的父节点的权重和
+ */
 #include <cstdio>
 #include <algorithm>
 
@@ -16,9 +24,9 @@ int head[MAXN], num, w[MAXN], n, size[MAXN];
 long long ans = INF, f[MAXN];
 
 inline void Add(int from, int to) {
-    e[++num].to = to;
-    e[num].next = head[from];
-    head[from] = num;
+    e[++num].to = to;   // 当前边指向的节点
+    e[num].next = head[from]; // 当前边的起始节点的上一条边的编号
+    head[from] = num; // 当前节点最后一条边的编号
 }
 
 void dfs(int u, int fa, int dep) {
